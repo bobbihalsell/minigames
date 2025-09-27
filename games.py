14cbf7b3591c8300d0b9de010ec0cd351e28cdb0
Bobbi <blh124@ic.ac.uk>
2025-09-27 14:51:52 +0100

    clean up and bugs

--- games.py
+++ games.py
@@ -4,19 +4,24 @@ import string
 from art import rps_pic, hangman_pic, treasure_pic
 import peopledata
 
-# guess the number computer chooses
 def guess_computers_num():
-    print('YOU ARE GUESSING THE COMPUTERS NUMBER')
-    bound = int(input('What should my number be between? 1 - ?  '))
-    valid_bound = isinstance(bound, int) and bound>1
-    while valid_bound==False:
-        bound = int(input('''
-        \nPLEASE SELECT AN INTEGER GREATER THAN 1
-        \rWhat should my number be between? 1 - ?  '''))
-        valid_bound = isinstance(bound, int) and bound>1
+    """Number guessing game where the player guesses the computer's number."""
+    print('\nYOU ARE GUESSING THE COMPUTERS NUMBER')
+    
+    while True:
+        try:
+            bound = int(input('What should my number be between? 1 - ? '))
+            if bound > 1:
+                break
+            else:
+                print('Please select an integer greater than 1')
+        except ValueError:
+            print('Please enter a valid number')
+    
     num = random.randint(1, bound)
     guess = 0
     guesses = 0
+    
     while guess != num:
         guess = int(input(f'Choose a number between 1 and {bound} '))
         if guess < num:
@@ -24,51 +29,73 @@ def guess_computers_num():
         elif guess > num:
             print('nope, too high.')
         guesses += 1
-    print(f'Congrats! you have guessed {num} correctly in {guesses} guesses!')
-
+    
+    print(f'Congratulations! You guessed {num} correctly in {guesses} guesses!')
 
 # guess the number you chooses
 def guess_your_num():
-    print('THE COMPUTER WILL GUESS YOUR NUMBER. CHOOSE A NUMBER.')
-    bound = int(input('What is your number between? 1 - ?  '))
-    valid_bound = isinstance(bound, int) and bound>1
-    while valid_bound==False:
-        bound = int(input('''
-        \nPLEASE SELECT AN INTEGER GREATER THAN 1
-        \rWhat is your number between? 1 - ?  '''))
-        valid_bound = isinstance(bound, int) and bound>1
+    """Number guessing game where the computer guesses the player's number."""
+    print('\nTHE COMPUTER WILL GUESS YOUR NUMBER')
+    print('Think of a number and I\'ll try to guess it!')
+    
+    while True:
+        try:
+            bound = int(input('What range is your number in? 1 - ? '))
+            if bound > 1:
+                break
+            else:
+                print('Please select an integer greater than 1')
+        except ValueError:
+            print('Please enter a valid number')
+    
     low = 1
     high = bound
-    feedback = ' '
+    feedback = ''
     guesses = 0
+    
+    print(f'\nThink of a number between 1 and {bound}!')
+    input('Press Enter when you\'re ready...')
+    
     while feedback != 'c':
         if low != high:
             guess = random.randint(low, high)
         else: 
             guess = low
-        feedback = input(f'''\n Is {guess} too high (H) too low (L) or correct (C)? ''').lower()
+            
+        guesses += 1
+        feedback = input(f'\nIs your number {guess}? (H)igher, (L)ower, or (C)orrect? ').lower().strip()
+        
+        while feedback not in ['h', 'l', 'c']:
+            feedback = input('Please enter H for higher, L for lower, or C for correct: ').lower().strip()
+        
         if feedback == 'h':
-            high = guess - 1
-            guesses += 1
-        elif feedback == 'l':
             low = guess + 1
-            guesses += 1
-    print(f'Yay, computer guessed your number, {guess}, correctly in {guesses} guesses!')
+        elif feedback == 'l':
+            high = guess - 1
+    
+    print(f'Yay! I guessed your number {guess} correctly in {guesses} guesses!')
 
 
 def rock_paper_scissors():
-    user = input("'r' for rock, 'p' for paper, 's' for scissors  ").lower()
+    """Play a single round of rock paper scissors."""
+    choices = {'r': 'Rock', 'p': 'Paper', 's': 'Scissors'}
+    
+    user = input("\n'r' for rock, 'p' for paper, 's' for scissors: ").lower().strip()
     while user not in ['r', 'p', 's']:
-        user = input('''
-        \n Please select a valid letter.
-        \r 'r' for rock, 'p' for paper, 's' for scissors  ''')
+        user = input("Please select a valid letter (r/p/s): ").lower().strip()
+    
     comp = random.choice(['r', 'p', 's'])
+    
+    print(f"\nYou chose: {choices[user]}")
+    print(f"Computer chose: {choices[comp]}")
+    
     if comp == 'r':
         print(rps_pic[0])
     elif comp == 'p':
         print(rps_pic[1])
     else:
         print(rps_pic[2])
+    
     if (user == 'r' and comp == 's') or (user == 'p' and comp == 'r') or (user == 's' and comp == 'p'):
         print('You won!')
         return 1
@@ -79,13 +106,16 @@ def rock_paper_scissors():
         print('tie')
         return 0
 
-def rps_best_of(x):
-    print(f'YOU ARE PLAYING ROCK PAPER SCISSORS. BEST OF {x}')
+def rps_best_of(rounds):
+    """Play a best-of-X series of rock paper scissors."""
+    print(f'YOU ARE PLAYING ROCK PAPER SCISSORS. BEST OF {rounds}')
     win = 0
     round = 1
-    while round <= x:
+    while round <= rounds:
+        print(f'round {round}.')
         win += rock_paper_scissors()
         round += 1
+        print(f'Score: You {(round-win)//2+max(win,0)} - {(round-win)//2+max(-win,0)} Computer')
     while win == 0:
         print('''\nITS A TIE!
                  \r play until someone wins''' )
@@ -96,18 +126,24 @@ def rps_best_of(x):
         print('YOU LOSE')
 
 def hangman():
-    print('YOU ARE PLAYING HANGMAN')
+    """Hangman word guessing game."""
+    print('\nHANGMAN GAME')
+    print('Guess the word one letter at a time!')
+    
     word = random.choice(word_list).upper()
     word_letters = set(word)
     alphabet = set(string.ascii_uppercase)
     guessed = set()
     tries = 6
-    while len(word_letters)>0:
-        letter_list = [l if l in guessed else '-' for l in word]
-        print(hangman_pic[6-tries])
-        print(' '.join(letter_list))
-        print('Letters used: ', ' '.join(guessed))
-        l = input(f'You have {tries} tries remaining. Guess a letter: ').upper()
+    while len(word_letters)>0 and tries > 0:
+        letter_list = [l if l in guessed else '_' for l in word]
+        
+        print('\n' + hangman_pic[6-tries])
+        print('Word: ' + ' '.join(letter_list))
+        print(f'Letters used: {" ".join(sorted(guessed)) if guessed else "None"}')
+        print(f'Remaining tries: {tries}')
+        
+        l = input('\nGuess a letter: ').upper().strip()
         if l in alphabet - guessed:
             guessed.add(l)
             if l in word_letters:
@@ -116,32 +152,152 @@ def hangman():
                 tries -=1
         elif l in guessed:
             print(f'You have already guessed {l}. Try again.')
+            continue
         elif l not in alphabet:
             print(f'please use a valid letter.')
-        if tries == 0:
-            print(f'Oh no, you ran out of tries, The word was {word}')
-            break
-    if len(word_letters)==0: 
+            continue
+        elif len(l) != 1:
+            print('Please enter only one letter.')
+            continue
+    
+    if tries == 0:
+        print(f'Oh no, you ran out of tries, The word was {word}')
+    else:
         print(f'''\n***************************************
                 \rCongrats! You guessed {word} correctly!
                 \r***************************************''')
 
 
 # tic tac toe
+def print_board(board):
+    """Print the tic-tac-toe board."""
+    print('\n   |   |   ')
+    print(f' {board[0]} | {board[1]} | {board[2]} ')
+    print('___|___|___')
+    print('   |   |   ')
+    print(f' {board[3]} | {board[4]} | {board[5]} ')
+    print('___|___|___')
+    print('   |   |   ')
+    print(f' {board[6]} | {board[7]} | {board[8]} ')
+    print('   |   |   ')
 
+def check_winner(board):
+    """Check if there's a winner on the tic-tac-toe board."""
+    winning_combinations = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],  # rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],  # columns
+        [0, 4, 8], [2, 4, 6]              # diagonals
+    ]
+    
+    for combo in winning_combinations:
+        if board[combo[0]] == board[combo[1]] == board[combo[2]] != ' ':
+            return board[combo[0]]
+    return None
 
+def get_computer_move(board):
+    """Get the computer's move using randomized AI."""
+    # Always check if computer can win first (highest priority)
+    win_moves = []
+    for i in range(9):
+        if board[i] == ' ':
+            board[i] = 'O'
+            if check_winner(board) == 'O':
+                win_moves.append(i)
+            board[i] = ' '
+    if win_moves:
+        return random.choice(win_moves)
+    
+    # Always check if computer needs to block player (second priority)
+    block_moves = []
+    for i in range(9):
+        if board[i] == ' ':
+            board[i] = 'X'
+            if check_winner(board) == 'X':
+                block_moves.append(i)
+            board[i] = ' '
+    if block_moves:
+        return random.choice(block_moves)
+    
+    # Strategic moves with randomness
+    available_moves = [i for i in range(9) if board[i] == ' ']
+    
+    # Sometimes take center (70% chance), sometimes be unpredictable
+    if board[4] == ' ' and random.random() < 0.7:
+        return 4
+    
+    # Take corners with randomization
+    corners = [0, 2, 6, 8]
+    available_corners = [corner for corner in corners if board[corner] == ' ']
+    if available_corners and random.random() < 0.6:
+        return random.choice(available_corners)
+    
+    # Take edges with some probability
+    edges = [1, 3, 5, 7]
+    available_edges = [edge for edge in edges if board[edge] == ' ']
+    if available_edges and random.random() < 0.3:
+        return random.choice(available_edges)
+    
+    # Take any available space (completely random)
+    return random.choice(available_moves)
+
+def tic_tac_toe():
+    """Play tic-tac-toe against the computer."""
+    print('\nTIC TAC TOE')
+    print('You are X, computer is O')
+    print('Positions are numbered 1-9:')
+    print('\n 1 | 2 | 3 ')
+    print('___|___|___')
+    print(' 4 | 5 | 6 ')
+    print('___|___|___')
+    print(' 7 | 8 | 9 ')
+    print('')
+    
+    board = [' '] * 9
+    current_player = 'X'
+    
+    for turn in range(9):
+        print_board(board)
         
+        if current_player == 'X':
+            while True:
+                try:
+                    move = int(input(f'\nEnter your move (1-9): ')) - 1
+                    if 0 <= move <= 8 and board[move] == ' ':
+                        board[move] = 'X'
+                        break
+                    else:
+                        print('Invalid move! Choose an empty position (1-9).')
+                except ValueError:
+                    print('Please enter a number between 1 and 9.')
+        else:
+            move = get_computer_move(board)
+            board[move] = 'O'
+            print(f'Computer chooses position {move + 1}')
+        
+        winner = check_winner(board)
+        if winner:
+            print_board(board)
+            if winner == 'X':
+                print('Congratulations! You won!')
+            else:
+                print('Computer wins! Better luck next time!')
+            return
+        
+        current_player = 'O' if current_player == 'X' else 'X'
+    
+    print_board(board)
+    print('It\'s a tie! Good game!')
 
-
-
-# Band name generator
+# Band Name Generator
 def band_name():
+    """Generate a random band name based on user input."""
     place = input("your favourite city you lived in?  ")
     animal = input("what pet do you have/ want?  ")
     print(f'Your band name is {place}s {animal}s!')
 
 # Love Calculator
 def love_calculator():
+    """Calculate love compatibility between two names."""
     print('WelcomE to the loOovEee CAalcuUlatOooOR')
     name1 = input('What is YOUR name?  ').lower()
     name2 = input ('What is THEIR name? ').lower()
@@ -163,6 +319,7 @@ def love_calculator():
 
 # Treasure hunt
 def treasure_island():
+    """Text-based adventure game to find treasure."""
     print(treasure_pic)
     print("Welcome to Treasure Island.")
     print("Your mission is to find the treasure.")
@@ -195,52 +352,68 @@ def treasure_island():
 
 # encrypt message
 def encrypt(message, shift):
-    alphabet_string = string.ascii_lowercase
-    alphabet = list(alphabet_string)
-    enc_mess = ""
+    """Encrypt a message using Caesar cipher."""
+    alphabet = string.ascii_lowercase
+    encrypted = ""
     for letter in message:
         if letter in alphabet:
             index = alphabet.index(letter)
-            new_index= (index + shift) % 26
-            enc_mess += alphabet[new_index]
+            new_index = (index + shift) % 26
+            encrypted += alphabet[new_index]
         else:
-            enc_mess += letter
-    return enc_mess
+            encrypted += letter
+    return encrypted
 
 # decrypt
 def decrypt(message, shift):
-    alphabet_string = string.ascii_lowercase
-    alphabet = list(alphabet_string)
-    dec_mess = ""
+    """Decrypt a message using Caesar cipher."""
+    alphabet = string.ascii_lowercase
+    decrypted = ""
     for letter in message:
         if letter in alphabet:
             index = alphabet.index(letter)
-            new_index= (index - shift) % 26
-            if new_index < 0:
-                new_index+= 26
-            dec_mess += alphabet[new_index]
+            new_index = (index - shift) % 26
+            decrypted += alphabet[new_index]
         else:
-            dec_mess += letter
-    return dec_mess
+            decrypted += letter
+    return decrypted
 
 # Ceasar Cypher
-def ceasar_cypher():
-    again =0
-    while again !='N':
-        choice = input('Type (E) to encrypt your message and (D) to decrypt your message ').upper().strip()
+def caesar_cipher():
+    """Caesar cipher encryption and decryption game."""
+    print('\nCAESAR CIPHER')
+    print('Encrypt and decrypt secret messages!')
+    
+    while True:
+        choice = input('\nType (E) to encrypt or (D) to decrypt: ').upper().strip()
         while choice not in ['E', 'D']:
-            choice = input('Please select a valid option, (E), (D).').upper().strip()
-        message = input('Type your message:\n').lower()
-        shift = int(input('Type the shift number\n'))
+            choice = input('Please select E or D: ').upper().strip()
+        
+        message = input('Enter your message: ').lower()
+        
+        while True:
+            try:
+                shift = int(input('Enter shift number (1-25): '))
+                if 1 <= shift <= 25:
+                    break
+                else:
+                    print('Please enter a number between 1 and 25')
+            except ValueError:
+                print('Please enter a valid number')
+        
         if choice == 'E':    
-            enc = encrypt(message, shift)
-            print(f'Your encrypted message is:\n{enc}')
-        if choice == 'D':
-            dec = decrypt(message, shift)
-            print(f'Your decrypted message is:\n{dec}')
-        again = input('Would you like to encrypt or decrypt again (Y/N)?\n').upper()
+            result = encrypt(message, shift)
+            print(f'Encrypted message: {result}')
+        else:
+            result = decrypt(message, shift)
+            print(f'Decrypted message: {result}')
+        
+        again = input('\nEncrypt/decrypt another message? (Y/N): ').upper().strip()
         while again not in ['Y', 'N']:
-            again = input('Please select Y or N?\n').upper()
+            again = input('Please enter Y or N: ').upper().strip()
+        
+        if again == 'N':
+            break
 
 
 # Black Jack
@@ -251,6 +424,7 @@ instructions = '''\nAn ACE can be 1 or 11.
                   \rIf the sum of your cards is higher than 21 BUST and you lose.'''
 
 def value_bj(cards):
+    """Calculate the value of a blackjack hand."""
     value = 0
     ace = False
     for i in range(len(cards)):
@@ -269,6 +443,7 @@ def value_bj(cards):
         
 
 def black_jack():
+    """Play a game of blackjack against the computer."""
     print('\nYOU ARE PLAYING BLACK JACK')
     print(instructions)
     players_cards = []
@@ -305,23 +480,25 @@ def black_jack():
 people = peopledata.data
 
 def random_person():
-    """chooses a random person"""
+    """Choose a random person from the celebrity data."""
     return random.choice(people)
 
 def format_data(person):
-    """Format account into printable format: name, description and country"""
+    """Format celebrity data into printable format."""
     name = person["name"]
     description = person["description"]
     country = person["country"]
     return f"{name}, a {description}, from {country}"
 
 def check_answer(answer, person1, person2):
+    """Check if the user's guess about follower counts is correct."""
     if person1['follower_count']>person2['follower_count']:
         return answer == 'A'
     else:
         return answer == 'B'
 
 def higher_lower():
+    """Guess which celebrity has more Instagram followers."""
     continue_game = True
     score = 0
     people_used=[]
@@ -348,3 +525,41 @@ def higher_lower():
             while personB in people_used:
                 personB = random.choice(people)
             people_used.append(personB)
+
+
+def word_scramble():
+    """Guess the word from its scrambled letters."""
+    print('\nWORD SCRAMBLE')
+    print('Unscramble the letters to guess the word!')
+    
+    word = random.choice(word_list).upper()
+    scrambled_word = ''.join(random.sample(word, len(word)))
+    
+    # Make sure the scrambled word is different from the original
+    while scrambled_word == word:
+        scrambled_word = ''.join(random.sample(word, len(word)))
+    
+    attempts = 3
+    print(f'\nScrambled word: {scrambled_word}')
+    print(f'Hint: This is a {len(word)}-letter word')
+    
+    while attempts > 0:
+        guess = input(f'\nYour guess (attempts left: {attempts}): ').upper().strip()
+        
+        if guess == word:
+            print(f'Correct! The word was "{word}"!')
+            print('Great job unscrambling!')
+            return
+        else:
+            attempts -= 1
+            if attempts > 0:
+                print(f'Not quite right. Try again!')
+                
+                # Give a hint after first wrong guess
+                if attempts == 2:
+                    print(f'Hint: The word starts with "{word[0]}"')
+                elif attempts == 1:
+                    print(f'Hint: The word ends with "{word[-1]}"')
+            else:
+                print(f'Game over! The word was "{word}"')
+                print('Better luck next time!')
--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import games
 import random
 
 def menu():
+    """Display the main game menu and get user's choice."""
     game_choice = '0'
-    menu_choices = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'B']
+    menu_choices = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', 'B']
     while game_choice not in menu_choices:
+        print("WELCOME TO THE MINI GAMES COLLECTION".center(60))
         game_choice = input('''
                         PLEASE SELECT A NUMBER TO PLAY A GAME
                         1) Number Guessing Game - YOU GUESS
@@ -14,47 +16,64 @@ def menu():
                         5) Random Band Name Generator
                         6) Love Calculator
                         7) Find the Treasure
-                        8) Ceasar Cipher
+                        8) Caesar Cipher
                         9) Black Jack
                         10) Higher or Lower - celebrities follower count
+                        11) Tic Tac Toe
+                        12) Word Scramble
                         B) BACK
         
-                            ''').upper()
+                            ''').upper().strip()
+        if game_choice not in menu_choices:
+            print("Invalid choice! Please select a valid option.")
     return game_choice
 
 def get_to_game(choice):
+    """Route the user to the selected game."""
     if choice != 'B': 
-        if choice == '1':
-            games.guess_computers_num()
-        elif choice == '2':
-            games.guess_your_num()
-        
-        elif choice == '3':
-            rounds = int(input('How many rounds would you like to play?  '))
-            games.rps_best_of(rounds)
+        try:
+            if choice == '1':
+                games.guess_computers_num()
+            elif choice == '2':
+                games.guess_your_num()
+            elif choice == '3':
+                while True:
+                    try:
+                        rounds = int(input('How many rounds would you like to play? '))
+                        if rounds > 0:
+                            break
+                        else:
+                            print("Please enter a positive number.")
+                    except ValueError:
+                        print("Please enter a valid number.")
+                games.rps_best_of(rounds)
+            elif choice == '4':
+                games.hangman()
+            elif choice == '5':
+                games.band_name()
+            elif choice == '6':
+                games.love_calculator()
+            elif choice == '7':
+                games.treasure_island()
+            elif choice == '8':
+                games.caesar_cipher()
+            elif choice == '9':
+                games.black_jack()
+            elif choice == '10':
+                games.higher_lower()
+            elif choice == '11':
+                games.tic_tac_toe()
+            elif choice == '12':
+                games.word_scramble()
+        except KeyboardInterrupt:
+            print("\n\nGame interrupted. Returning to menu...")
+            return choice
+        except Exception as e:
+            print(f"\nAn error occurred: {e}")
+            print("Returning to menu...")
+            return choice
 
-        elif choice == '4':
-            games.hangman()
-
-        elif choice == '5':
-            games.band_name()
-        
-        elif choice == '6':
-            games.love_calculator()
-        
-        elif choice == '7':
-            games.treasure_island()
-        
-        elif choice == '8':
-            games.ceasar_cypher()
-        
-        elif choice == '9':
-            games.black_jack()
-            
-        elif choice == '10':
-            games.higher_lower()
-
-        again = input("\nTo play again type '1' otherwise select any button to return to the main menu")
+        again = input("\nTo play again type '1' otherwise press any key to return to the main menu: ").strip()
         if again == '1':
             get_to_game(choice)
     return choice
@@ -62,19 +81,50 @@ def get_to_game(choice):
 
 
 def play():
+    print("      WELCOME TO THE MINI GAMES COLLECTION!")
     name = input('Hello, what is your name? ')
     choice = 1
     while choice != 'E':
-        choice = input(f'\nHello {name}, would you like to see the game menu (M) or select a game randomly (R) or exit (E)?  ').upper()
+        print(f"\nHello {name}! What would you like to do?")
+        choice = input(f'''
+(M) - Browse game menu
+(R) - Play a random game  
+(E) - Exit
+        
+Your choice: ''').upper().strip()
+        
         while choice not in ['M', 'R', 'E']:
-            choice = input("\nPlease select 'M' for menu or 'R' for a randomly selected game or 'E' to exit ").upper()
+            choice = input("\nPlease select 'M' for menu, 'R' for random game, or 'E' to exit: ").upper().strip()
+        
         if choice == 'M':
             game_choice = menu()
             choice = get_to_game(game_choice)
         elif choice == 'R':
-            random_choice = random.choice(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'])
+            random_choice = random.choice(['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'])
+            print(f"Randomly selected game: {get_game_name(random_choice)}")
             choice = get_to_game(random_choice)
-    print('GOODBYE')
+    
+    print(f'\nGoodbye {name}! Thanks for playing!')
 
 
-play()
+def get_game_name(choice):
+    """Get the name of a game based on its choice number."""
+    game_names = {
+        '1': 'Number Guessing Game - YOU GUESS',
+        '2': 'Number Guessing Game - YOU CHOOSE',
+        '3': 'Rock Paper Scissors',
+        '4': 'Hangman',
+        '5': 'Random Band Name Generator',
+        '6': 'Love Calculator',
+        '7': 'Find the Treasure',
+        '8': 'Caesar Cipher',
+        '9': 'Black Jack',
+        '10': 'Higher or Lower',
+        '11': 'Tic Tac Toe',
+        '12': 'Word Scramble'
+    }
+    return game_names.get(choice, 'Unknown Game')
+
+
+if __name__ == "__main__":
+    play()
